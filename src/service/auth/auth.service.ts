ffb71578712144080e0472ea838ef8e38188f7e4
thanhoaiedunlu <21130360@st.hcmuaf.edu.vn>
2025-06-01 10:14:09 +0700

    login

---
+++ src/service/auth/auth.service.ts
@@ -0,0 +1,30 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import { Router } from '@angular/router';
+import {Observable} from 'rxjs';
+import {environment} from '../../environments/environment.development';
+import { jwtDecode } from 'jwt-decode';
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  constructor(private httpClient: HttpClient,
+              private router: Router
+  ) {}
+  userData: any = {};
+  sendLoginForm(data: object): Observable<any> {
+    return this.httpClient.post(
+      `${environment.API_DOMAIN}/api/auth/login`,
+      data
+    );
+  }
+  saveUserInfo() {
+    if (localStorage.getItem('token') !== null) {
+      this.userData = jwtDecode(localStorage.getItem('token')!);
+    }
+  }
+  signOut(): void {
+    localStorage.removeItem('token');
+    this.router.navigate(['/login']);
+  }
+}
